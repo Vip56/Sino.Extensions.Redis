76ca6cf2d144b07cada57f1f80357b7f43b9daef
yaozhenfa <976691141@qq.com>
2019-01-08 17:20:08 +0800

    移除不必要的特性

--- src/Sino.Extensions.Redis/Internal/IO/RedisIO.cs
+++ src/Sino.Extensions.Redis/Internal/IO/RedisIO.cs
@@ -8,15 +8,12 @@ namespace Sino.Extensions.Redis.Internal.IO
     {
         readonly RedisWriter _writer;
         RedisReader _reader;
-        RedisPipeline _pipeline;
         BufferedStream _stream;
 
         public RedisWriter Writer { get { return _writer; } }
         public RedisReader Reader { get { return GetOrThrow(_reader); } }
         public Encoding Encoding { get; set; }
-        public RedisPipeline Pipeline { get { return GetOrThrow(_pipeline); } }
         public Stream Stream { get { return GetOrThrow(_stream); } }
-        public bool IsPipelined { get { return Pipeline == null ? false : Pipeline.Active; } }
 
         public RedisIO()
         {
@@ -27,15 +24,12 @@ namespace Sino.Extensions.Redis.Internal.IO
         public void SetStream(Stream stream)
         {
             _stream?.Dispose();
-
             _stream = new BufferedStream(stream);
             _reader = new RedisReader(this);
-            _pipeline = new RedisPipeline(this);
         }
 
         public void Dispose()
         {
-            _pipeline?.Dispose();
             _stream?.Dispose();
         }
 
