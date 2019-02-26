46f81c4871fa6586c978decb341cae788732fd52
yaozhenfa <976691141@qq.com>
2019-02-26 16:47:42 +0800

    增加其他必要方法

--- src/Sino.Serializer.Abstractions/SerializerSettingsBuilder.cs
+++ src/Sino.Serializer.Abstractions/SerializerSettingsBuilder.cs
@@ -11,11 +11,15 @@ namespace Sino.Serializer.Abstractions
     /// </summary>
     public class SerializerSettingsBuilder
     {
-        private ConcurrentDictionary<string, Action<IConvertProvider>> _convertProviders;
+        private ConcurrentDictionary<string, Func<IConvertProvider>> _convertProviders;
+
+        public Encoding GlobalEncoding = Encoding.UTF8;
+
+        public string DefaultConvertProviderName { get; private set; }
 
         public SerializerSettingsBuilder()
         {
-            _convertProviders = new ConcurrentDictionary<string, Action<IConvertProvider>>();
+            _convertProviders = new ConcurrentDictionary<string, Func<IConvertProvider>>();
         }
 
         /// <summary>
@@ -23,7 +27,7 @@ namespace Sino.Serializer.Abstractions
         /// </summary>
         /// <param name="name">提供器名称</param>
         /// <param name="convertProvider">提供器对象</param>
-        public void AddProvider(string name, Action<IConvertProvider> convertProvider)
+        public void AddProvider(string name, Func<IConvertProvider> convertProvider)
         {
             if (_convertProviders.ContainsKey(name))
                 throw new ConvertProviderExistedException($"The {name} ConvertProvider is exited");
@@ -32,6 +36,30 @@ namespace Sino.Serializer.Abstractions
             {
                 return convertProvider;
             });
+        }
+
+        /// <summary>
+        /// 移除序列化提供器
+        /// </summary>
+        /// <param name="name"></param>
+        public void RemoveProvider(string name)
+        {
+            if (!_convertProviders.ContainsKey(name))
+                return;
+
+            Func<IConvertProvider> provider = null;
+            _convertProviders.TryRemove(name, out provider);
+        }
+
+        /// <summary>
+        /// 设置默认的序列化提供器
+        /// </summary>
+        /// <param name="name"></param>
+        public void SetDefaultConvertProvider(string name)
+        {
+            if (!_convertProviders.ContainsKey(name))
+                throw new KeyNotFoundException($"The {name} ConvertProvider is not existed");
+            DefaultConvertProviderName = name;
         }
     }
 }
