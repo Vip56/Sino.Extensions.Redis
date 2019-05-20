05d2adb3883c5afefb5bfa63a7c487610c577dc4
姚正发 <yaozhengfa@vip56.cn>
2019-05-20 17:37:02 +0800

    增加ProtoBuf项目

---
+++ test/SerializerUnitTest/JsonUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializerUnitTest
+{
+    public class JsonUnitTest : BaseUnitTest
+    {
+        public JsonUnitTest()
+            : base(x =>
+             {
+             })
+        { }
+
+
+    }
+}
